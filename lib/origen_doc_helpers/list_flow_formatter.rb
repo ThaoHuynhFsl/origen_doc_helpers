ffd0609a3f0b69381a4956ee362bb3433b4c93f1
Jiang Liu <jiang.liu@nxp.com>
2022-06-15 15:23:38 -0700

    updated for c28esf3 compatibility

--- lib/origen_doc_helpers/list_flow_formatter.rb
+++ lib/origen_doc_helpers/list_flow_formatter.rb
@@ -13,8 +13,22 @@ module OrigenDocHelpers
       html
     end
 
-    def open_table
+    def open_table(headers_and_attributes: :default)
       str = ''
+      #if headers_and_attributes == :default
+      #  str << "<table class=\"table table-hover\">"
+      #  str << '<thead><tr>'
+      #  str << '<th>Number</th>'
+      #  str << '<th>Result</th>'
+      #  str << '<th>Test</th>'
+      #  str << '<th>Pattern</th>'
+      #  str << '<th>Bin</th>'
+      #  str << '<th>Softbin</th>'
+      #  str << '</tr></thead>'
+      #else
+      #  str << headers_and_attributes
+      #end
+
       str << "<table class=\"table table-striped\" style=\"table-layout: fixed; width: 100%\">"
       str << "<table class=\"table table-bordered\">"
       str << '<thead><tr>'
@@ -52,7 +66,7 @@ module OrigenDocHelpers
       html << "<tr><td colspan=\"6\"><strong>RENDER: </strong> An expicitly rendered flow snippet occurs here</td></tr>"
     end
 
-    def on_test(node)
+    def on_test(node, body_and_attributes: :default)
       id = node.find(:id).value
       html << "<tr id=\"list_#{@flow}_test_#{id}\" class=\"list-test-line clickable\" data-testid=\"flow_#{@flow}_test_#{id}\">"
       # sequence id
@@ -80,17 +94,16 @@ module OrigenDocHelpers
       end
       html << "<td width=\"35%\" style=\"word-break: break-word\">#{name}</td>"
       html << "<td width=\"20%\" style=\"word-break: break-word\">#{node.find(:object).value['Pattern']}</td>"
-      vddhv = node.value.fetch('Vddhv').to_s.upcase
-      vddlv = node.value.fetch('Vddlv').to_s.upcase
       # if no limits found in node
       if node.find_all(:limit).nil? || node.find_all(:limit).empty?
         html << '<td></td>'
-        html << '<td></td>'
-        html << '<td></td>'   
+        html << '<td></td>' 
       else # found parametric limits in node
         para_limits = node.find_all(:limit)
         u_limit = ''
+        u_limit_units = ''
         l_limit = ''
+        l_limit_units = ''
         para_limits[0..1].each do |limit|
           if limit.inspect.include?('gte')
             l_limit = '%.4g' % limit.value
@@ -100,13 +113,35 @@ module OrigenDocHelpers
             raise 'Cannot idenfity if the value of limit is for upper or lower limit'
           end
         end
-        force_val = node.find_all(:force_value).first.value.round(3) # assume 1 force value for now
-        html << "<td>#{l_limit.to_s << node.find_all(:units).first.value}</td>"
-        html << "<td>#{u_limit.to_s << node.find_all(:units).first.value}</td>"
-        html << "<td>#{force_val}</td>"
+
+        if !node.find_all(:units).empty?
+          u_limit_units = node.find_all(:units).first.value 
+          l_limit_units = node.find_all(:units).first.value 
+        end
+        html << "<td>#{l_limit.to_s << l_limit_units}</td>" 
+        html << "<td>#{u_limit.to_s << l_limit_units}</td>" 
+      end
+
+      # force value
+      force_val = ''
+      force_val = node.find_all(:force_value).first.value.round(3) if !node.find_all(:force_value).empty? # assume 1 force value for now
+      html << "<td>#{force_val}</td>"
+
+      # vddhv and vddlv
+      vddhv = ''
+      vddlv = ''
+      if node.find(:object).value.include?('Vdio')
+        vddhv = node.value.fetch('Vdio').to_s.upcase
+      else
+        vddhv = node.value.fetch('Vddhv').to_s.upcase
+      end
+      if node.find(:object).value.include?('Vddx')
+        vddlv = node.value.fetch('Vddx').to_s.upcase
+      else
+        vddlv = node.value.fetch('Vddlv').to_s.upcase
       end
       html << "<td>#{vddhv}</td>"
-      html << "<td>#{vddlv}</td>"   
+      html << "<td>#{vddlv}</td>"  
       if node.description
         html << "<td width=\"35%\" style=\"word-break: break-word\">#{node.description.join("\n")}</td>" 
       else
